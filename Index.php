d7fdbf8c103c43407634388331582e88109dfeee
moonloverx2 <332322385@qq.com>
2015-12-28 13:42:12 +0800

    添加sqmrty引擎
    添加sqmrty引擎

--- Index.php
+++ Index.php
@@ -1,54 +1,11 @@
-<html> 
-<head> 
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312" /> 
-<title>PHP MVC留言板</title> 
-</head> 
-<body> 
-<a href="post.htm">添加新留言</a><br> 
-<p>  
-    
-   
-<?php 
-//!index.php 总入口 
-/** 
-* index.php的 调用形式为： 
-* 显示所有留言：index.php?action=list 
-* 添加留 言 ：index.php?action=post 
-* 删除留言 ：index.php?action=delete& id=x 
-*/
-require_once('DataAccess.php'); 
-require_once('Model.php'); 
-require_once('View.php'); 
-require_once('Controller.php'); 
-//创建DataAccess对象（请根据你的需要修改参数值） 
-$dao=& new DataAccess ('localhost','root','','test'); 
-//根据$_GET["action"]取值的不同调用不同的控制器子类 
-$action=$_GET["action"];  
-    
-   
-switch ( 
-   
-$action) 
-{ 
-case "post": 
-$controller=& new postController($dao,$_POST); break; 
-case "list": 
-$controller=& new listController($dao); break; 
-case "delete": 
-$controller=& new deleteController($dao,$_GET["id"]); break; 
-default: 
-$controller=& new listController($dao); break; //默认为显示留言  
-    
-   
-    
-   
-}  
-    
-   
-    
-   
-$view=$controller->getView(); //获取视图对象 
-$view->display(); //输出HTML 
-?> 
-</body> 
-</html>
+<?php
+require_once 'libs/smarty/Smarty.class.php';
+$smarty = new Smarty();
+$smarty->left_delimiter = "{";
+$smarty->right_delimiter = "}";
+$smarty->template_dir = "tpl";
+$smarty->compile_dir = "tpl_c";
+$smarty->cache_dir = "dir";
+$smarty->assign("title","title测试");
+$smarty->display("tpl/Index.tpl");
+?>
---
+++ tpl_c/e34463f80b13a2336c99531b4aa67c2d27d6776f_0.file.Index.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/* Smarty version 3.1.29, created on 2015-12-28 12:29:58
+  from "D:\LNMP\nginx\nginx-1.6.2\html\MVC\tpl\Index.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_5680bac67f13a7_45073404',
+  'file_dependency' => 
+  array (
+    'e34463f80b13a2336c99531b4aa67c2d27d6776f' => 
+    array (
+      0 => 'D:\\LNMP\\nginx\\nginx-1.6.2\\html\\MVC\\tpl\\Index.tpl',
+      1 => 1451276961,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5680bac67f13a7_45073404 ($_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['title']->value;
+}
+}
